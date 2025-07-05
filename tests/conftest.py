434fef1f405e698e1df9ea8368d08ab53d73a037
Keith Carlson Jr. <keith.carlson@hhs.gov>
2025-07-05 17:21:04 -0400

    Fix test fixtures and e2e response check

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,19 +8,25 @@ from rtree import index
 
 # creates a temp CWD with a minimal runs.pkl and yields
 @pytest.fixture(scope="session", autouse=True)
-def temp_dataset(tmp_path_factory, monkeypatch):
+def temp_dataset(tmp_path_factory):
     tmpdir = tmp_path_factory.mktemp("rheat")
     pkl = tmpdir / "runs.pkl"
     sample = {1: {"bbox": (-1, -1, 1, 1), "geoms": {}, "metadata": {}}}
     pkl.write_bytes(pickle.dumps(sample))
-    monkeypatch.chdir(tmpdir)
+
+    orig = os.getcwd()
+    os.chdir(tmpdir)
+
     # reconfigure the app to use this temporary dataset
     flask_app.RUNS_PKL_PATH = str(pkl)
     flask_app.runs = sample
     flask_app.idx = index.Index()
     for rid, run in sample.items():
         flask_app.idx.insert(rid, run["bbox"])
+
     yield
+
+    os.chdir(orig)
 
 @pytest.fixture
 def client():
